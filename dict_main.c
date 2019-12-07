519fe694787fa9bc3710a80d0dd0456493487787
Brad Hong <bsh3424@uw.edu>
2019-12-06 19:51:15 -0800

    debugging 5

--- dict_main.c
+++ dict_main.c
@@ -4,7 +4,7 @@
 
 #include "dict.h"
 
-int main(int argc, char *argv[argc+1]) {
+int main(int argc, char *argv[argc + 1]) {
   struct dict_t *dict = dictionary_new(argv[2], 5e5);
   if (!strcmp(argv[1], "gen")) {
     if (dictionary_generate(dict, argv[3]) < 0) {
@@ -12,14 +12,14 @@ int main(int argc, char *argv[argc+1]) {
       return EXIT_FAILURE;
     }
   } else if (!strcmp(argv[1], "load")) {
-    if(!dictionary_load(dict)) {
+    if (!dictionary_load(dict)) {
       printf("Error loading dictionary\n");
       return EXIT_FAILURE;
     }
   }
   char *addr = dictionary_exists(dict, "cat");
   if (addr) {
-    printf("i haz cat, it is at: %p\n", (void*)addr);
+    printf("i haz cat, it is at: %p\n", (void *)addr);
   }
   dictionary_close(dict);
   return EXIT_SUCCESS;
